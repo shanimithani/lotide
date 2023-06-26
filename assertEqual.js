c5d0ac886e1045a0cd59159fde9073f8ed5549a2
Shani Mithani <shanimithani@gmail.com>
2023-06-26 15:40:58 -0400

    Comparing two outputs

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "✅✅✅ Assertion passed: " + actual + " === " + expected;
+  } else {
+    return "🛑🛑🛑 Assertion failed: " + actual + " !== " + expected;
+  }
+};
+
+// TEST CODE
+
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("hi", "hi"));
+console.log(assertEqual("2", 2));
