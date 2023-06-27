224ea27a9bfb4292e7b1e75abcd5ec4e737abd76
Shani Mithani <shanimithani@gmail.com>
2023-06-27 18:01:56 -0400

    Remove words from arrays

---
+++ without.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  let match = true;
+  if (array1.length !== array2.length) {
+    match = false;
+  } else {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        match = false;
+      }
+    }
+  }
+  return match;
+};
+
+const assertEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    return `✅✅✅ Assertion passed: ${array1} === ${array2}`;
+  } else {
+    return `🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`;
+  }
+};
+
+const without = function(values, toRemove) {
+  let newArray = [];
+  for (let x = 0; x < values.length; x++) {
+    if (!toRemove.includes(values[x])) {
+      newArray.push(values[x]);
+    }
+  }
+  return newArray;
+};
+
+const words = ["hello", "world", "lighthouse"];
+let newArray = without(words, ["lighthouse"]);
+// Make sure the original array was not altered by the without function
+console.log(assertEqual(words, ["hello", "world", "lighthouse"]));
+console.log(newArray);
