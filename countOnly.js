cfee6edf3f3329dd5779242d1d4cef8c0c5dbb89
Shani Mithani <shanimithani@gmail.com>
2023-06-28 15:03:41 -0400

    Only count values that are true in an object

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`;
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1; //It exists and you are incrementing 
+      } else {
+        results[item] = 1; //You're adding to the results object
+      }
+    }
+  }
+  return results;
+}
+
+///Test code
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
