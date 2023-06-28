6ab62c67348ed9b2b5fd1b547a88a1d45e9bc346
Shani Mithani <shanimithani@gmail.com>
2023-06-28 15:29:28 -0400

    Return index of each letter in sentence

---
+++ letterPositions.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`;
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (!results[letter]) {
+        results[letter] = [i];
+      } else {
+        results[letter].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
