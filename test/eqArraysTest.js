d78211e9a5382199e6c4e011010caa3d97d113d8
Shani Mithani <shanimithani@gmail.com>
2023-07-03 13:54:28 -0400

    Refactored eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); //true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); //false
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false)); //false
