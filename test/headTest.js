5dce6a5562b77b150f6b807716975461155ad521
Shani Mithani <shanimithani@gmail.com>
2023-07-03 14:22:02 -0400

    Incorporated mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head(["Hello"]), "Hello"));
-console.log(assertEqual(head([]), undefined));
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 'Hello' if its in the middle of the array", () => {
+    assert.strictEqual(head(['Hello','Lighthouse','Labs']), 'Hello'); 
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns undefined for empty array", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+});
