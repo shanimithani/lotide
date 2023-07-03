e1c200c5449a7ee3e831ae1d062493629c896bba
Shani Mithani <shanimithani@gmail.com>
2023-07-03 14:55:39 -0400

    More mocha and chai refactoring for tail and middle functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-console.log(assertArraysEqual(middle([1,2,3,4,5]),[3]));
-console.log(assertArraysEqual(middle([1,2,3,4]),[2,3]));
+
+describe("#middle", () => {
+  it("one element for odd array", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it("two elements for even array", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require('../assertEqual');
+/*const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 console.log(assertEqual(result.length, 2)); // ensure we get back two elements
 console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs" */
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+const result = ["Hello", "Lighthouse", "Labs"];
+
+describe("#tail", () => {
+  it("ensure we get back two elements", () => {
+    assert.strictEqual(tail(result).length, 2);
+  });
+  it("ensure first element is Lighthouse and 2nd element is labs", () => {
+    assert.deepEqual(tail(result), ['Lighthouse','Labs']); 
+  });
+
+});
